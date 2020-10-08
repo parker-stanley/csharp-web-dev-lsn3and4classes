4f495f2aa31db8e666b327776ae436e3208c76be
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-10-08 19:37:42 -0400

    first couple tasks

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+
+        private string courseName;
+        private string courseDescription;
+        private Dictionary<string, string> syllabus; 
+
+        public Course()
+        {
+
+        }
+    }
+}
